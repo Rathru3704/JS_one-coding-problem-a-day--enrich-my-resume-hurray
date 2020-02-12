cf8fafb16a734757d01218b9ced26fa18f4d779c
Rathru3704 <chan0433@algonquinlive.com>
2020-02-11 21:46:47 -0500

    Two Sum

---
+++ 01_twoSum/01_twoSum.js
@@ -0,0 +1,13 @@
+var twoSum = function(nums, target) {
+    for(let i = 0; i < nums.length; i++){
+        for(let j = i + 1; j < nums.length; j++){
+            if(nums[j] === target - nums[i]){
+                return[i, j];
+            }
+        }
+    }
+};
+
+twoSum([2, 7, 11, 15], 9);
+
+// [0, 1]
