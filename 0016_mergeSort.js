f119474b1b97ea69ce9df796346ba621643ade1e
Rathru3704 <chan0433@algonquinlive.com>
2020-04-07 12:16:23 -0400

    update comment

--- 0016_mergeSort.js
+++ 0016_mergeSort.js
@@ -27,4 +27,4 @@ const mergeSort = (arr) => {  // Split the array into halves and merge them recu
 console.log(mergeSort([3,44,38,5,47,15,36,26,27,2,46,4,19,50,48]));
 // [ 2, 3, 4, 5, 15, 19, 26, 27, 36, 38, 44, 46, 47, 48, 50 ]
 console.log(mergeSort([27,4,37,44,16,28,7,18,41,1]));
-// 
+// [ 1, 4, 7, 16, 18, 27, 28, 37, 41, 44 ]
