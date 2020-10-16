2fd41528f3dd2da968fc22e38eda2b0f67953455
Rathru3704 <chan0433@algonquinlive.com>
2020-10-15 22:23:22 -0400

    Sum Zero Update

--- Multiple-Pointers/0026_Sum-Zero.js
+++ Multiple-Pointers/0026_Sum-Zero.js
@@ -22,6 +22,7 @@ const sumZero = (arr) => {
 console.log(sumZero([-3,-2,-1,0,1,2,3]));  // [-3, 3]
 console.log(sumZero([-2,0,1,3]));  // undefined
 console.log(sumZero([1,2,3]));  //undefined
+console.log(sumZero([1,5,-3,6,8,3,-2,1]));  //undefined
 
 
 
